6979b3eacee04037b46af9abac3314ec59fef68f
tobeck <tobias.becker.olsson@gmail.com>
2023-02-06 13:57:55 +0100

    Upd prof pic, rm boiler plate meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,33 +4,18 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Tobias Becker'
+const name = 'TSBO'
 export const siteTitle = 'TSBO Gaming'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <header className={styles.header}>
         {home ? (
           <>
             <Image
               priority
-              src="/images/tobias_becker_profile.jpg"
+              src="/images/profile_picture.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +28,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/tobias_becker_profile.jpg"
+                src="/images/profile_picture.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
