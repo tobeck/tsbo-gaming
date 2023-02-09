c03c949313a1a1cd55fe124238c4685cc132edad
tobeck <tobias.becker.olsson@gmail.com>
2023-02-09 09:52:15 +0100

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ module.exports = {
     screens: {
       xs: '540px',
     },
-    extend: {},
+    colors: {
+      golden: '#FFD700',
+      parchment_bg: '#E8DFA9',
+      brown_bg: '#4E4B39',
+      text_on_brown: '#E8DFA9',
+      text_on_parchment: '#4E4B39',
+      text_gold: '#FFD700',
+    },
+    extend: {
+      backgroundImage: {
+        hero_bg_image: "url('../../public/images/tsbo_title_bg.png')",
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
