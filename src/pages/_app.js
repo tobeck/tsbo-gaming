6f7b9583daba5b10c1f789d29dc64edd15e6bdd0
tobeck <tobias.becker.olsson@gmail.com>
2023-02-10 15:21:18 +0100

    Imported next-seo, added basic SEO parameters

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="TSBO Gaming"
+        description="TSBO Gaming is a strategy and survival game blog and tutorial site."
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://tsbogaming.com/',
+          siteName: 'tsbogaming',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 
@@ -24,6 +25,31 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <>
+      <NextSeo
+        title={postData.title}
+        description={postData.title}
+        //canonical="www.example.com/next-seo-blog"
+        openGraph={{
+          type: 'article',
+          article: {
+            //publishedTime: '2022-06-21T23:04:13Z',
+            //modifiedTime: '2022-01-21T18:04:43Z',
+            //authors: [
+            //  'https://www.example.com/authors/@firstnameA-lastnameA',
+            //  'https://www.example.com/authors/@firstnameB-lastnameB',
+            //],
+            //tags: ['Tag A', 'Tag B', 'Tag C'],
+          },
+          //url: 'www.example.com/next-seo-blog',
+          // images: {
+          //   url: 'https://www.test.ie/images/cover.jpg',
+          //   width: 850,
+          //   height: 650,
+          //   alt: 'Photo of text',
+          // },
+          site_name: 'TSBO Gaming',
+        }}
+      />
       <Head>
         <title>{postData.title}</title>
       </Head>
